737198ca955731f829269d12fb652ae1d14363d7
Lionel Santoro <lionelsanto2004@gmail.com>
2026-04-08 16:08:19 -0300

    Se configuraron las vistas principales, rutas y barra de navegacion
    - Se crearon las 5 vistas base del e-commerce (Página Principal, Quiénes Somos, Comercialización, Contacto, Términos y Usos).
    - Se definieron las rutas correspondientes en web.php para la navegación.

---
+++ resources/views/InformacionDeContacto.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head> 
+    
+    <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}"> 
+</head> 
+<body>
+
+    <div class="container mt-5">
+        <div class="alert alert-success" role="alert">
+            <p>BIENVENIDO</p>
+        </div>
+    </div>
+     <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script> 
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,22 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('PaginaPrincipal');
+});
+
+Route::get('/quienes-somos', function () {
+    return view('QuienesSomos');
+});
+
+Route::get('/comercializacion', function () {
+    return view('Comercializacion');
+});
+
+
+Route::get('/informacion-de-contacto', function () {
+    return view('InformacionDeContacto');
+});
+
+Route::get('/terminos-y-usos', function () {
+    return view('Terminos_Y_Usos');
 });
