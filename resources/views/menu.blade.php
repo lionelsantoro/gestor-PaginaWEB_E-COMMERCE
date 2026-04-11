b628431be8ad58b3b3fcec557baffa3c61b02ab1
MRmarcos7869 <Rojasmarcos7505@gmail.com>
2026-04-11 00:04:40 -0300

    agrege la pagina catalogo

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -2,6 +2,7 @@
     <div class="container">
         <a class="navbar-brand" href="/">Fradega</a>
         <div class="navbar-nav">
+            <a class="nav-link" href="/catologo">Catalogo</a>
             <a class="nav-link" href="/quienes-somos">Quiénes Somos</a>
             <a class="nav-link" href="/comercializacion">Comercialización</a>
         </div>
