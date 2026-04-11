f069c8043886961a4dc4d426b112ba1191917463
MRmarcos7869 <Rojasmarcos7505@gmail.com>
2026-04-10 22:07:15 -0300

    modifique el menu, quite pagina principal y ahora donde dice fradega direcciona a la pagina principal

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -2,9 +2,8 @@
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a class="navbar-brand" href="#">Mi Sitio</a>
+        <a class="navbar-brand" href="/">Fradega</a>
         <div class="navbar-nav">
-            <a class="nav-link" href="/">Página Principal</a>
             <a class="nav-link" href="/quienes-somos">Quiénes Somos</a>
             <a class="nav-link" href="/comercializacion">Comercialización</a>
             <a class="nav-link" href="/informacion-de-contacto">Información de Contacto</a>
