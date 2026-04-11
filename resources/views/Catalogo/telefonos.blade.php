ee3f15d4e1f6c7646760be40428d0fb440623f65
MRmarcos7869 <Rojasmarcos7505@gmail.com>
2026-04-11 00:32:42 -0300

    cree paginas para cada producto del catalogo

---
+++ resources/views/Catalogo/telefonos.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+
+    <head> 
+        <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}"> 
+    </head> 
+    
+    <body class="d-flex flex-column min-vh-100">
+
+        @include('menu')
+
+        <div class="container mt-5">
+            <div class="alert alert-success" role="alert">
+                
+            </div>
+        </div>
+
+        @include('piedepagina')
+
+        <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script> 
+    
+    </body>
+
+</html>
