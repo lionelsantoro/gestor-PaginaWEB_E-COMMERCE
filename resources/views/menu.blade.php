fe0fd0009448a2bdef820ac6ca0fddcc3b1ad5d1
MRmarcos7869 <Rojasmarcos7505@gmail.com>
2026-04-11 00:17:17 -0300

    agrege un submenu en catalogo

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -2,7 +2,16 @@
     <div class="container">
         <a class="navbar-brand" href="/">Fradega</a>
         <div class="navbar-nav">
-            <a class="nav-link" href="/catologo">Catalogo</a>
+            <div class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Catálogo
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Teléfonos</a></li>
+                    <li><a class="dropdown-item" href="#">Computadoras</a></li>
+                    <li><a class="dropdown-item" href="#">Electrodomésticos</a></li>
+                </ul>
+            </div>
             <a class="nav-link" href="/quienes-somos">Quiénes Somos</a>
             <a class="nav-link" href="/comercializacion">Comercialización</a>
         </div>
