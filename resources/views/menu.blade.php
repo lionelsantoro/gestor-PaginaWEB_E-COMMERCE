5d4d0c81f6351263503c1389c1b37d46ec51f359
MRmarcos7869 <Rojasmarcos7505@gmail.com>
2026-04-10 23:49:05 -0300

    quite los terminos de uso y la info de contacto del menu para ponerlo en el pie de pagina

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -4,8 +4,6 @@
         <div class="navbar-nav">
             <a class="nav-link" href="/quienes-somos">Quiénes Somos</a>
             <a class="nav-link" href="/comercializacion">Comercialización</a>
-            <a class="nav-link" href="/informacion-de-contacto">Información de Contacto</a>
-            <a class="nav-link" href="/terminos-y-usos">Términos y Usos</a>
         </div>
     </div>
 </nav>
