055ae63d4af8442351dc7f33dbb243d8a6a94162
MRmarcos7869 <Rojasmarcos7505@gmail.com>
2026-04-18 20:36:57 -0300

    cree el catalogo de lavarropas

---
+++ resources/views/Catalogo/lavarropas2.blade.php
@@ -0,0 +1,290 @@
+<!DOCTYPE html>
+<html lang="es">
+
+    <head> 
+        <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}"> 
+    </head> 
+    
+    <body class="d-flex flex-column min-vh-100">
+
+        @include('menu')
+
+    <div class="container mt-5">
+
+  <div class="row row-cols-1 row-cols-md-3 g-4">
+    
+    <div class="col">
+      <div class="card h-100">
+        <img src="/Imagenes Celulares/FOTO1.webp" class="card-img-top" alt="SAMSUNG GALAXY S26 ULTRA" style="height: 250px; object-fit: contain;">
+        <div class="card-body">
+          <h5 class="card-title"><strong>SAMSUNG GALAXY S26 ULTRA</strong></h5>
+          <p class="fs-4 fw-bold mb-3">PRECIO: $2.800.000</p>
+          <p class="card-text">
+            <strong>Almacenamiento:</strong> 512 GB<br>
+            <strong>Stock:</strong> 15 disponibles<br>
+            <strong>Medios de pago:</strong> Efectivo, Visa, Mastercard, American Express, desde 6 hasta 12 cuotas sin interés.<br>
+            <strong>Envío:</strong> Gratis
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="card h-100">
+        <img src="/Imagenes Celulares/FOTO2.webp" class="card-img-top" alt="APPLE IPHONE 17 PRO MAX" style="height: 250px; object-fit: contain;">
+        <div class="card-body">
+          <h5 class="card-title"><strong>APPLE IPHONE 17 PRO MAX</strong></h5>
+          <p class="fs-4 fw-bold mb-3">PRECIO: $3.100.000</p>
+          <p class="card-text">
+            <strong>Almacenamiento:</strong> 512 GB<br>
+            <strong>Stock:</strong> 10 disponibles<br>
+            <strong>Medios de pago:</strong> Efectivo, Visa, Mastercard, Naranja X, desde 3 hasta 9 cuotas sin interés.<br>
+            <strong>Envío:</strong> Gratis
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="card h-100">
+        <img src="/Imagenes Celulares/FOTO3.webp" class="card-img-top" alt="MOTOROLA EDGE 60 ULTRA" style="height: 250px; object-fit: contain;">
+        <div class="card-body">
+          <h5 class="card-title"><strong>MOTOROLA EDGE 60 ULTRA</strong></h5>
+          <p class="fs-4 fw-bold mb-3">PRECIO: $1.900.000</p>
+          <p class="card-text">
+            <strong>Almacenamiento:</strong> 256 GB<br>
+            <strong>Stock:</strong> 22 disponibles<br>
+            <strong>Medios de pago:</strong> Efectivo, Mastercard, Visa, Cabal, desde 6 hasta 18 cuotas sin interés.<br>
+            <strong>Envío:</strong> $15.000
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="card h-100">
+        <img src="/Imagenes Celulares/FOTO4.webp" class="card-img-top" alt="SAMSUNG GALAXY Z FOLD 7" style="height: 250px; object-fit: contain;">
+        <div class="card-body">
+          <h5 class="card-title"><strong>SAMSUNG GALAXY Z FOLD 7</strong></h5>
+          <p class="fs-4 fw-bold mb-3">PRECIO: $3.500.000</p>
+          <p class="card-text">
+            <strong>Almacenamiento:</strong> 1 TB<br>
+            <strong>Stock:</strong> 5 disponibles<br>
+            <strong>Medios de pago:</strong> Efectivo, Visa, American Express, Cabal, desde 12 hasta 24 cuotas sin interés.<br>
+            <strong>Envío:</strong> Gratis
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="card h-100">
+        <img src="/Imagenes Celulares/FOTO5.jpg" class="card-img-top" alt="APPLE IPHONE 16 PRO" style="height: 250px; object-fit: contain;">
+        <div class="card-body">
+          <h5 class="card-title"><strong>APPLE IPHONE 16 PRO</strong></h5>
+          <p class="fs-4 fw-bold mb-3">PRECIO: $2.100.000</p>
+          <p class="card-text">
+            <strong>Almacenamiento:</strong> 128 GB<br>
+            <strong>Stock:</strong> 15 disponibles<br>
+            <strong>Medios de pago:</strong> Efectivo, Mastercard, Visa, American Express, desde 6 hasta 12 cuotas sin interés.<br>
+            <strong>Envío:</strong> Gratis
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="card h-100">
+        <img src="/Imagenes Celulares/FOTO6.jpg" class="card-img-top" alt="MOTOROLA RAZR 60 ULTRA" style="height: 250px; object-fit: contain;">
+        <div class="card-body">
+          <h5 class="card-title"><strong>MOTOROLA RAZR 60 ULTRA</strong></h5>
+          <p class="fs-4 fw-bold mb-3">PRECIO: $2.100.000</p>
+          <p class="card-text">
+            <strong>Almacenamiento:</strong> 512 GB<br>
+            <strong>Stock:</strong> 8 disponibles<br>
+            <strong>Medios de pago:</strong> Efectivo, American Express, Naranja X, Visa, desde 6 hasta 12 cuotas sin interés.<br>
+            <strong>Envío:</strong> $18.000
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="card h-100">
+        <img src="/Imagenes Celulares/FOTO7.webp" class="card-img-top" alt="SAMSUNG GALAXY A26" style="height: 250px; object-fit: contain;">
+        <div class="card-body">
+          <h5 class="card-title"><strong>SAMSUNG GALAXY A26</strong></h5>
+          <p class="fs-4 fw-bold mb-3">PRECIO: $450.000</p>
+          <p class="card-text">
+            <strong>Almacenamiento:</strong> 128 GB<br>
+            <strong>Stock:</strong> 70 disponibles<br>
+            <strong>Medios de pago:</strong> Efectivo, Mastercard, American Express, Visa, desde 3 hasta 6 cuotas sin interés.<br>
+            <strong>Envío:</strong> $6.000
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="card h-100">
+        <img src="/Imagenes Celulares/FOTO8.jfif" class="card-img-top" alt="APPLE IPHONE SE4" style="height: 250px; object-fit: contain;">
+        <div class="card-body">
+          <h5 class="card-title"><strong>APPLE IPHONE SE4</strong></h5>
+          <p class="fs-4 fw-bold mb-3">PRECIO: $1.100.000</p>
+          <p class="card-text">
+            <strong>Almacenamiento:</strong> 128 GB<br>
+            <strong>Stock:</strong> 28 disponibles<br>
+            <strong>Medios de pago:</strong> Efectivo, Visa, Cabal, Mastercard, desde 3 hasta 6 cuotas sin interés.<br>
+            <strong>Envío:</strong> $9.000
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="card h-100">
+        <img src="/Imagenes Celulares/FOTO9.webp" class="card-img-top" alt="XIAOMI 15 PRO" style="height: 250px; object-fit: contain;">
+        <div class="card-body">
+          <h5 class="card-title"><strong>XIAOMI 15 PRO</strong></h5>
+          <p class="fs-4 fw-bold mb-3">PRECIO: $2.100.000</p>
+          <p class="card-text">
+            <strong>Almacenamiento:</strong> 512 GB<br>
+            <strong>Stock:</strong> 18 disponibles<br>
+            <strong>Medios de pago:</strong> Efectivo, Visa, Mastercard, Naranja X, desde 3 hasta 9 cuotas sin interés.<br>
+            <strong>Envío:</strong> Gratis
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="card h-100">
+        <img src="/Imagenes Celulares/FOTO10.jpg" class="card-img-top" alt="GOOGLE PIXEL 10 PRO" style="height: 250px; object-fit: contain;">
+        <div class="card-body">
+          <h5 class="card-title"><strong>GOOGLE PIXEL 10 PRO</strong></h5>
+          <p class="fs-4 fw-bold mb-3">PRECIO: $2.300.000</p>
+          <p class="card-text">
+            <strong>Almacenamiento:</strong> 256 GB<br>
+            <strong>Stock:</strong> 12 disponibles<br>
+            <strong>Medios de pago:</strong> Efectivo, Visa, American Express, Cabal, desde 3 hasta 6 cuotas sin interés.<br>
+            <strong>Envío:</strong> Gratis
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="card h-100">
+        <img src="/Imagenes Celulares/FOTO11.webp" class="card-img-top" alt="HUAWEI PURA 80 PRO" style="height: 250px; object-fit: contain;">
+        <div class="card-body">
+          <h5 class="card-title"><strong>HUAWEI PURA 80 PRO</strong></h5>
+          <p class="fs-4 fw-bold mb-3">PRECIO: $2.400.000</p>
+          <p class="card-text">
+            <strong>Almacenamiento:</strong> 512 GB<br>
+            <strong>Stock:</strong> 8 disponibles<br>
+            <strong>Medios de pago:</strong> Efectivo, Mastercard, Cabal, Visa, desde 6 hasta 12 cuotas sin interés.<br>
+            <strong>Envío:</strong> Gratis
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="card h-100">
+        <img src="/Imagenes Celulares/FOTO12.webp" class="card-img-top" alt="OPPO FIND X8 PRO" style="height: 250px; object-fit: contain;">
+        <div class="card-body">
+          <h5 class="card-title"><strong>OPPO FIND X8 PRO</strong></h5>
+          <p class="fs-4 fw-bold mb-3">PRECIO: $2.200.000</p>
+          <p class="card-text">
+            <strong>Almacenamiento:</strong> 512 GB<br>
+            <strong>Stock:</strong> 14 disponibles<br>
+            <strong>Medios de pago:</strong> Efectivo, Naranja X, Mastercard, Visa, desde 3 hasta 12 cuotas sin interés.<br>
+            <strong>Envío:</strong> $12.000
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="card h-100">
+        <img src="/Imagenes Celulares/FOTO13.jfif" class="card-img-top" alt="XIAOMI REDMI NOTE 15 PRO+" style="height: 250px; object-fit: contain;">
+        <div class="card-body">
+          <h5 class="card-title"><strong>XIAOMI REDMI NOTE 15 PRO+</strong></h5>
+          <p class="fs-4 fw-bold mb-3">PRECIO: $950.000</p>
+          <p class="card-text">
+            <strong>Almacenamiento:</strong> 256 GB<br>
+            <strong>Stock:</strong> 30 disponibles<br>
+            <strong>Medios de pago:</strong> Efectivo, Visa, Mastercard, American Express, desde 3 hasta 6 cuotas sin interés.<br>
+            <strong>Envío:</strong> $8.000
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="card h-100">
+        <img src="/Imagenes Celulares/FOTO14.webp" class="card-img-top" alt="GOOGLE PIXEL 10A" style="height: 250px; object-fit: contain;">
+        <div class="card-body">
+          <h5 class="card-title"><strong>GOOGLE PIXEL 10A</strong></h5>
+          <p class="fs-4 fw-bold mb-3">PRECIO: $1.200.000</p>
+          <p class="card-text">
+            <strong>Almacenamiento:</strong> 128 GB<br>
+            <strong>Stock:</strong> 25 disponibles<br>
+            <strong>Medios de pago:</strong> Efectivo, Visa, Cabal, Naranja X, desde 3 hasta 9 cuotas sin interés.<br>
+            <strong>Envío:</strong> $10.000
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="card h-100">
+        <img src="/Imagenes Celulares/FOTO15.webp" class="card-img-top" alt="OPPO RENO 13 PRO" style="height: 250px; object-fit: contain;">
+        <div class="card-body">
+          <h5 class="card-title"><strong>OPPO RENO 13 PRO</strong></h5>
+          <p class="fs-4 fw-bold mb-3">PRECIO: $1.450.000</p>
+          <p class="card-text">
+            <strong>Almacenamiento:</strong> 256 GB<br>
+            <strong>Stock:</strong> 20 disponibles<br>
+            <strong>Medios de pago:</strong> Efectivo, Mastercard, Visa, Naranja X, desde 3 hasta 6 cuotas sin interés.<br>
+            <strong>Envío:</strong> $9.000
+          </p>
+        </div>
+      </div>
+    </div>
+
+  </div> </div>
+
+
+
+
+<nav aria-label="Navegación de páginas">
+  <ul class="pagination justify-content-center mt-5 mb-5">
+    
+    <li class="page-item disabled">
+      <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Anterior</a>
+    </li>
+    
+    <li class="page-item active" aria-current="page">
+      <a class="page-link" href="/catalogo/telefonos">1</a>
+    </li>
+    
+    <li class="page-item">
+      <a class="page-link" href="/catalogo/telefonos/2">2</a>
+    </li>
+    
+    <li class="page-item">
+      <a class="page-link" href="/catalogo/telefonos/2">Siguiente</a>
+    </li>
+    
+  </ul>
+</nav>
+
+
+        @include('piedepagina')
+
+        <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script> 
+    
+    </body>
+
+</html>
