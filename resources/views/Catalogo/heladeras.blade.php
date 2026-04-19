10fa4fd1dbfaa4746f49782f07fe5c3a72c4564c
MRmarcos7869 <Rojasmarcos7505@gmail.com>
2026-04-18 22:00:16 -0300

    cree los archivos de heladeras

---
+++ resources/views/Catalogo/heladeras.blade.php
@@ -0,0 +1,331 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}">
+</head>
+
+<body class="d-flex flex-column min-vh-100">
+
+  @include('menu')
+
+  <div class="container mt-5">
+
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+
+      <div class="col">
+        <div class="card h-100">
+          <img src="/imagenes lavarropas/foto1.webp" class="card-img-top" alt="LAVARROPAS DREAN NEXT 8.14"
+            style="height: 250px; object-fit: contain;">
+          <div class="card-body">
+            <h5 class="card-title fs-4"><strong>LAVARROPAS DREAN NEXT 8.14</strong></h5>
+            <p class="fs-5 fw-bold mb-3">PRECIO: $850.000</p>
+            <p class="card-text">
+              <strong>Capacidad de lavado:</strong> 8 kg<br>
+              <strong>Programas de lavado:</strong> 14<br>
+              <strong>Stock:</strong> 15 disponibles<br>
+              <strong>Medios de pago:</strong> Efectivo, Visa, Mastercard, American Express, desde 6 hasta 12 cuotas sin
+              interés.<br>
+              <strong>Envío:</strong> Gratis
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card h-100">
+          <img src="/imagenes lavarropas/foto2.webp" class="card-img-top" alt="LAVARROPAS SAMSUNG INVERTER ECOBUBBLE"
+            style="height: 250px; object-fit: contain;">
+          <div class="card-body">
+            <h5 class="card-title fs-4"><strong>LAVARROPAS SAMSUNG INVERTER ECOBUBBLE</strong></h5>
+            <p class="fs-5 fw-bold mb-3">PRECIO: $1.250.000</p>
+            <p class="card-text">
+              <strong>Capacidad de lavado:</strong> 9.5 kg<br>
+              <strong>Programas de lavado:</strong> 22<br>
+              <strong>Stock:</strong> 10 disponibles<br>
+              <strong>Medios de pago:</strong> Efectivo, Visa, Mastercard, Naranja X, desde 3 hasta 9 cuotas sin
+              interés.<br>
+              <strong>Envío:</strong> Gratis
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card h-100">
+          <img src="/imagenes lavarropas/foto3.webp" class="card-img-top" alt="LAVARROPAS WHIRLPOOL CARGA SUPERIOR"
+            style="height: 250px; object-fit: contain;">
+          <div class="card-body">
+            <h5 class="card-title fs-4"><strong>LAVARROPAS WHIRLPOOL CARGA SUPERIOR</strong></h5>
+            <p class="fs-5 fw-bold mb-3">PRECIO: $780.000</p>
+            <p class="card-text">
+              <strong>Capacidad de lavado:</strong> 9 kg<br>
+              <strong>Programas de lavado:</strong> 10<br>
+              <strong>Stock:</strong> 22 disponibles<br>
+              <strong>Medios de pago:</strong> Efectivo, Mastercard, Visa, Cabal, desde 6 hasta 18 cuotas sin
+              interés.<br>
+              <strong>Envío:</strong> $15.000
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card h-100">
+          <img src="/imagenes lavarropas/foto4.webp" class="card-img-top" alt="LAVARROPAS LG DIRECT DRIVE"
+            style="height: 250px; object-fit: contain;">
+          <div class="card-body">
+            <h5 class="card-title fs-4"><strong>LAVARROPAS LG DIRECT DRIVE</strong></h5>
+            <p class="fs-5 fw-bold mb-3">PRECIO: $1.450.000</p>
+            <p class="card-text">
+              <strong>Capacidad de lavado:</strong> 10.5 kg<br>
+              <strong>Programas de lavado:</strong> 14<br>
+              <strong>Stock:</strong> 5 disponibles<br>
+              <strong>Medios de pago:</strong> Efectivo, Visa, American Express, Cabal, desde 12 hasta 24 cuotas sin
+              interés.<br>
+              <strong>Envío:</strong> Gratis
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card h-100">
+          <img src="/imagenes lavarropas/foto5.jpg" class="card-img-top" alt="LAVARROPAS PHILCO AUTOMÁTICO"
+            style="height: 250px; object-fit: contain;">
+          <div class="card-body">
+            <h5 class="card-title fs-4"><strong>LAVARROPAS PHILCO AUTOMÁTICO</strong></h5>
+            <p class="fs-5 fw-bold mb-3">PRECIO: $690.000</p>
+            <p class="card-text">
+              <strong>Capacidad de lavado:</strong> 7 kg<br>
+              <strong>Programas de lavado:</strong> 8<br>
+              <strong>Stock:</strong> 15 disponibles<br>
+              <strong>Medios de pago:</strong> Efectivo, Mastercard, Visa, American Express, desde 6 hasta 12 cuotas sin
+              interés.<br>
+              <strong>Envío:</strong> Gratis
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card h-100">
+          <img src="/imagenes lavarropas/foto6.jpg" class="card-img-top" alt="LAVARROPAS MIDEA ECO INVERTER"
+            style="height: 250px; object-fit: contain;">
+          <div class="card-body">
+            <h5 class="card-title fs-4"><strong>LAVARROPAS MIDEA ECO INVERTER</strong></h5>
+            <p class="fs-5 fw-bold mb-3">PRECIO: $920.000</p>
+            <p class="card-text">
+              <strong>Capacidad de lavado:</strong> 8 kg<br>
+              <strong>Programas de lavado:</strong> 16<br>
+              <strong>Stock:</strong> 8 disponibles<br>
+              <strong>Medios de pago:</strong> Efectivo, American Express, Naranja X, Visa, desde 6 hasta 12 cuotas sin
+              interés.<br>
+              <strong>Envío:</strong> $18.000
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card h-100">
+          <img src="/imagenes lavarropas/foto7.webp" class="card-img-top" alt="LAVARROPAS GAFA ACQUARIUS"
+            style="height: 250px; object-fit: contain;">
+          <div class="card-body">
+            <h5 class="card-title fs-4"><strong>LAVARROPAS GAFA ACQUARIUS</strong></h5>
+            <p class="fs-5 fw-bold mb-3">PRECIO: $550.000</p>
+            <p class="card-text">
+              <strong>Capacidad de lavado:</strong> 6.5 kg<br>
+              <strong>Programas de lavado:</strong> 6<br>
+              <strong>Stock:</strong> 70 disponibles<br>
+              <strong>Medios de pago:</strong> Efectivo, Mastercard, American Express, Visa, desde 3 hasta 6 cuotas sin
+              interés.<br>
+              <strong>Envío:</strong> $6.000
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card h-100">
+          <img src="/imagenes lavarropas/foto8.jfif" class="card-img-top" alt="LAVARROPAS CANDY SMART"
+            style="height: 250px; object-fit: contain;">
+          <div class="card-body">
+            <h5 class="card-title fs-4"><strong>LAVARROPAS CANDY SMART</strong></h5>
+            <p class="fs-5 fw-bold mb-3">PRECIO: $810.000</p>
+            <p class="card-text">
+              <strong>Capacidad de lavado:</strong> 9 kg<br>
+              <strong>Programas de lavado:</strong> 15<br>
+              <strong>Stock:</strong> 28 disponibles<br>
+              <strong>Medios de pago:</strong> Efectivo, Visa, Cabal, Mastercard, desde 3 hasta 6 cuotas sin
+              interés.<br>
+              <strong>Envío:</strong> $9.000
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card h-100">
+          <img src="/imagenes lavarropas/foto9.webp" class="card-img-top" alt="LAVARROPAS LONGVIE PREMIUM"
+            style="height: 250px; object-fit: contain;">
+          <div class="card-body">
+            <h5 class="card-title fs-4"><strong>LAVARROPAS LONGVIE PREMIUM</strong></h5>
+            <p class="fs-5 fw-bold mb-3">PRECIO: $980.000</p>
+            <p class="card-text">
+              <strong>Capacidad de lavado:</strong> 8 kg<br>
+              <strong>Programas de lavado:</strong> 12<br>
+              <strong>Stock:</strong> 18 disponibles<br>
+              <strong>Medios de pago:</strong> Efectivo, Visa, Mastercard, Naranja X, desde 3 hasta 9 cuotas sin
+              interés.<br>
+              <strong>Envío:</strong> Gratis
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card h-100">
+          <img src="/imagenes lavarropas/foto10.jpg" class="card-img-top" alt="LAVARROPAS ELECTROLUX PREMIUM PLUS"
+            style="height: 250px; object-fit: contain;">
+          <div class="card-body">
+            <h5 class="card-title fs-4"><strong>LAVARROPAS ELECTROLUX PREMIUM PLUS</strong></h5>
+            <p class="fs-5 fw-bold mb-3">PRECIO: $1.150.000</p>
+            <p class="card-text">
+              <strong>Capacidad de lavado:</strong> 11 kg<br>
+              <strong>Programas de lavado:</strong> 10<br>
+              <strong>Stock:</strong> 12 disponibles<br>
+              <strong>Medios de pago:</strong> Efectivo, Visa, American Express, Cabal, desde 3 hasta 6 cuotas sin
+              interés.<br>
+              <strong>Envío:</strong> Gratis
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card h-100">
+          <img src="/imagenes lavarropas/foto11.webp" class="card-img-top" alt="LAVARROPAS AURORA"
+            style="height: 250px; object-fit: contain;">
+          <div class="card-body">
+            <h5 class="card-title fs-4"><strong>LAVARROPAS AURORA</strong></h5>
+            <p class="fs-5 fw-bold mb-3">PRECIO: $620.000</p>
+            <p class="card-text">
+              <strong>Capacidad de lavado:</strong> 6 kg<br>
+              <strong>Programas de lavado:</strong> 11<br>
+              <strong>Stock:</strong> 8 disponibles<br>
+              <strong>Medios de pago:</strong> Efectivo, Mastercard, Cabal, Visa, desde 6 hasta 12 cuotas sin
+              interés.<br>
+              <strong>Envío:</strong> Gratis
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card h-100">
+          <img src="/imagenes lavarropas/foto12.webp" class="card-img-top" alt="LAVARROPAS PATRICK CLÁSICO"
+            style="height: 250px; object-fit: contain;">
+          <div class="card-body">
+            <h5 class="card-title fs-4"><strong>LAVARROPAS PATRICK CLÁSICO</strong></h5>
+            <p class="fs-5 fw-bold mb-3">PRECIO: $480.000</p>
+            <p class="card-text">
+              <strong>Capacidad de lavado:</strong> 5 kg<br>
+              <strong>Programas de lavado:</strong> 7<br>
+              <strong>Stock:</strong> 14 disponibles<br>
+              <strong>Medios de pago:</strong> Efectivo, Naranja X, Mastercard, Visa, desde 3 hasta 12 cuotas sin
+              interés.<br>
+              <strong>Envío:</strong> $12.000
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card h-100">
+          <img src="/imagenes lavarropas/foto13.jfif" class="card-img-top" alt="LAVARROPAS BGH CARGA FRONTAL"
+            style="height: 250px; object-fit: contain;">
+          <div class="card-body">
+            <h5 class="card-title fs-4"><strong>LAVARROPAS BGH CARGA FRONTAL</strong></h5>
+            <p class="fs-5 fw-bold mb-3">PRECIO: $720.000</p>
+            <p class="card-text">
+              <strong>Capacidad de lavado:</strong> 7 kg<br>
+              <strong>Programas de lavado:</strong> 15<br>
+              <strong>Stock:</strong> 30 disponibles<br>
+              <strong>Medios de pago:</strong> Efectivo, Visa, Mastercard, American Express, desde 3 hasta 6 cuotas sin
+              interés.<br>
+              <strong>Envío:</strong> $8.000
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card h-100">
+          <img src="/imagenes lavarropas/foto14.webp" class="card-img-top" alt="LAVARROPAS HISENSE INVERTER"
+            style="height: 250px; object-fit: contain;">
+          <div class="card-body">
+            <h5 class="card-title fs-4"><strong>LAVARROPAS HISENSE INVERTER</strong></h5>
+            <p class="fs-5 fw-bold mb-3">PRECIO: $880.000</p>
+            <p class="card-text">
+              <strong>Capacidad de lavado:</strong> 8.5 kg<br>
+              <strong>Programas de lavado:</strong> 16<br>
+              <strong>Stock:</strong> 25 disponibles<br>
+              <strong>Medios de pago:</strong> Efectivo, Visa, Cabal, Naranja X, desde 3 hasta 9 cuotas sin interés.<br>
+              <strong>Envío:</strong> $10.000
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card h-100">
+          <img src="/imagenes lavarropas/foto15.webp" class="card-img-top" alt="LAVARROPAS KOH-I-NOOR"
+            style="height: 250px; object-fit: contain;">
+          <div class="card-body">
+            <h5 class="card-title fs-4"><strong>LAVARROPAS KOH-I-NOOR</strong></h5>
+            <p class="fs-5 fw-bold mb-3">PRECIO: $950.000</p>
+            <p class="card-text">
+              <strong>Capacidad de lavado:</strong> 7.5 kg<br>
+              <strong>Programas de lavado:</strong> 15<br>
+              <strong>Stock:</strong> 20 disponibles<br>
+              <strong>Medios de pago:</strong> Efectivo, Mastercard, Visa, Naranja X, desde 3 hasta 6 cuotas sin
+              interés.<br>
+              <strong>Envío:</strong> $9.000
+            </p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+  <nav aria-label="Navegación de páginas">
+    <ul class="pagination justify-content-center mt-5 mb-5">
+
+      <li class="page-item disabled">
+        <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Anterior</a>
+      </li>
+
+      <li class="page-item active" aria-current="page">
+        <a class="page-link" href="/catalogo/heladeras">1</a>
+      </li>
+
+      <li class="page-item">
+        <a class="page-link" href="/catalogo/heladeras/2">2</a>
+      </li>
+
+      <li class="page-item">
+        <a class="page-link" href="/catalogo/heladeras/2">Siguiente</a>
+      </li>
+
+    </ul>
+  </nav>
+
+  @include('piedepagina')
+
+  <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+</body>
+
+</html>
