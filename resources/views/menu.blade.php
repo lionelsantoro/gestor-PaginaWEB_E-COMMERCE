1c6413735cf27d2e0764db501164bc0b64052040
MRmarcos7869 <Rojasmarcos7505@gmail.com>
2026-04-25 20:25:39 -0300

    agrege una imagen con el logo de fravega en el menu y unos estilos de boostrap

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,24 +1,26 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #7828D8;">
     <div class="container">
-        <a class="navbar-brand" href="/">Frávega</a>
-        <div class="navbar-nav">
-            <a class="nav-link" href="/">Inicio</a>
+        <a class="navbar-brand" href="/">
+            <img src="/Imagenes inicio/foto0.png" height="50" alt="Logo de la marca">
+        </a>        
+        <div class="navbar-nav" style="font-size: 1.2rem;"> 
+            <a class="nav-link px-3 fw-semibold" href="/">Inicio</a> 
             <div class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <a class="nav-link dropdown-toggle px-3 fw-semibold" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                     Catálogo
                 </a>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="/catalogo/telefonos">Teléfonos</a></li>
-                    <li><a class="dropdown-item" href="/catalogo/computadoras">Computadoras</a></li>
-                    <li><a class="dropdown-item" href="/catalogo/lavarropas">Lavarropas</a></li>
-                    <li><a class="dropdown-item" href="/catalogo/heladeras">Heladeras</a></li>
+                <ul class="dropdown-menu dropdown-menu-dark" style="background-color: #7828D8; border: none;">
+                    <li><a class="dropdown-item fw-semibold" href="/catalogo/telefonos">Teléfonos</a></li>
+                    <li><a class="dropdown-item fw-semibold" href="/catalogo/computadoras">Computadoras</a></li>
+                    <li><a class="dropdown-item fw-semibold" href="/catalogo/lavarropas">Lavarropas</a></li>
+                    <li><a class="dropdown-item fw-semibold" href="/catalogo/heladeras">Heladeras</a></li>
                 </ul>
             </div>
-            <a class="nav-link" href="/quienes-somos">Quiénes Somos</a>
-            <a class="nav-link" href="/comercializacion">Comercialización</a>
-            <a class="nav-link" href="/terminos-y-usos" >Términos y Usos</a> | 
-            <a class="nav-link" href="/informacion-de-contacto" >Contacto</a>
-            <a class="nav-link" href="/login" >Iniciar Sesion</a>
+            <a class="nav-link px-3 fw-semibold" href="/quienes-somos">Quiénes Somos</a>
+            <a class="nav-link px-3 fw-semibold" href="/comercializacion">Comercialización</a>
+            <a class="nav-link px-3 fw-semibold" href="/terminos-y-usos">Términos y Usos</a>
+            <a class="nav-link px-3 fw-semibold" href="/informacion-de-contacto">Contacto</a>
+            <a class="nav-link px-3 fw-semibold" href="/login">Iniciar Sesion</a>
         </div>
     </div>
 </nav>
