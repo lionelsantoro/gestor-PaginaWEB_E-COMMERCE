5bfaf83b3ef398d8d69a2987d24e72fb230ddc77
lionelsantoro <lionelsanto2004@gmail.com>
2026-04-26 00:54:53 -0300

    Modifiqué el diseño de la barra de navegación.

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -2,25 +2,52 @@
     <div class="container">
         <a class="navbar-brand" href="/">
             <img src="/Imagenes inicio/foto0.png" height="50" alt="Logo de la marca">
-        </a>        
-        <div class="navbar-nav" style="font-size: 1.2rem;"> 
-            <a class="nav-link px-3 fw-semibold" href="/">Inicio</a> 
-            <div class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle px-3 fw-semibold" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Catálogo
-                </a>
-                <ul class="dropdown-menu dropdown-menu-dark" style="background-color: #7828D8; border: none;">
-                    <li><a class="dropdown-item fw-semibold" href="/catalogo/telefonos">Teléfonos</a></li>
-                    <li><a class="dropdown-item fw-semibold" href="/catalogo/computadoras">Computadoras</a></li>
-                    <li><a class="dropdown-item fw-semibold" href="/catalogo/lavarropas">Lavarropas</a></li>
-                    <li><a class="dropdown-item fw-semibold" href="/catalogo/heladeras">Heladeras</a></li>
-                </ul>
-            </div>
-            <a class="nav-link px-3 fw-semibold" href="/quienes-somos">Quiénes Somos</a>
-            <a class="nav-link px-3 fw-semibold" href="/comercializacion">Comercialización</a>
-            <a class="nav-link px-3 fw-semibold" href="/terminos-y-usos">Términos y Usos</a>
-            <a class="nav-link px-3 fw-semibold" href="/informacion-de-contacto">Contacto</a>
-            <a class="nav-link px-3 fw-semibold" href="/login">Iniciar Sesion</a>
+        </a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav ms-auto" style="font-size: 1.1rem;">
+                
+                <li class="nav-item">
+                    <a class="nav-link px-3 fw-semibold active" aria-current="page" href="/">Inicio</a>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle px-3 fw-semibold" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Catálogo
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-dark" style="background-color: #7828D8; border: none;">
+                        <li><a class="dropdown-item fw-semibold" href="/catalogo/telefonos">Teléfonos</a></li>
+                        <li><a class="dropdown-item fw-semibold" href="/catalogo/computadoras">Computadoras</a></li>
+                        <li><a class="dropdown-item fw-semibold" href="/catalogo/lavarropas">Lavarropas</a></li>
+                        <li><a class="dropdown-item fw-semibold" href="/catalogo/heladeras">Heladeras</a></li>
+                    </ul>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link px-3 fw-semibold" href="/quienes-somos">Quiénes Somos</a>
+                </li>
+                
+                <li class="nav-item">
+                    <a class="nav-link px-3 fw-semibold" href="/comercializacion">Comercialización</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link px-3 fw-semibold" href="/terminos-y-usos">Términos y Usos</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link px-3 fw-semibold" href="/informacion-de-contacto">Contacto</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link px-3 fw-semibold" href="/login">Iniciar Sesion</a>
+                </li>
+
+            </ul>
         </div>
     </div>
 </nav>
