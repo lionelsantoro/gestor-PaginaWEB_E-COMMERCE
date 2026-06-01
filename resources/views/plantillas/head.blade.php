a91d472f06dca78a577f778e4215d5623e5a6aa9
MRmarcos7869 <Rojasmarcos7505@gmail.com>
2026-06-01 12:04:28 -0300

    agrege un icono a las pestanas de todas las paginas

--- resources/views/plantillas/head.blade.php
+++ resources/views/plantillas/head.blade.php
@@ -2,6 +2,9 @@
     <meta charset="UTF-8">
     <title>{{ $titulo ?? 'PáginaPrincipal' }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="icon" href="{{ asset('/Imagenes inicio/foto00.png') }}" type="image/x-icon">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}">
