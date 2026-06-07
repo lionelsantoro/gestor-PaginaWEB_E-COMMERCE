f8f123981bb7537494ee77fbd79edfdc991452f4
MRmarcos7869 <Rojasmarcos7505@gmail.com>
2026-06-06 23:07:01 -0300

    ajuste un problema con los estilos de boostrap de la carpeta vendor

--- resources/views/carrito/index.blade.php
+++ resources/views/carrito/index.blade.php
@@ -268,6 +268,8 @@
     @include('plantillas.piedepagina')
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script> 
+
 
     <script>
         document.addEventListener('DOMContentLoaded', function () {
