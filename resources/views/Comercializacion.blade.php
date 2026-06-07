8448089e9f63b4a860ead8d4623d706235c36af8
MRmarcos7869 <Rojasmarcos7505@gmail.com>
2026-06-06 23:32:54 -0300

    ajsute el menu desplegable de la vista de admin

--- resources/views/Comercializacion.blade.php
+++ resources/views/Comercializacion.blade.php
@@ -503,8 +503,8 @@
 
         @include('plantillas.piedepagina')
 
-        <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script> 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script> 
     
     </body>
 
